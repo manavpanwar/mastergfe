b9955020ff14e9c98a1e4bbf6beecb4ab0bf6f3e
manavpanwar <manav.panwar318@gmail.com>
2026-04-16 07:11:52 +0530

    chnes

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  allowedDevOrigins: ["masterg.vizta.in"],
+}
 export default nextConfig;
